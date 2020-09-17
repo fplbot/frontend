795456eacdfc04b54b48cdb33c3531b17af77f91
John Korsnes <johnkors@gmail.com>
2020-09-17 11:23:09 +0200

    Add GA tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,18 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const gaId = process.env.GOOGLE_ANALYTICS_ID;
   return (
     <>
       <Head>
+        {gaId && <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />}
+        {gaId && <script dangerouslySetInnerHTML={{
+          __html: ` window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments)}
+              gtag('js', new Date());
+              gtag('config', '${gaId}');`
+        }}
+        />}
         <title>fplbot</title>
       </Head>
       <Component {...pageProps} />
