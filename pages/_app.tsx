16a4d80180186668b53725caf0f62dccceaae787
John Korsnes <johnkors@gmail.com>
2020-09-17 10:16:57 +0200

    Adds Twitter Share meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>fplbot</title>
+        <meta name="theme-color" content="#37003c" />
+        <meta name="slack-app-id" content="AREFP62B1" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@fplbotapp" />
+        <meta name="twitter:creator" content="@fplbotapp" />
+        <meta property="og:url" content="https://www.fplbot.app" />
+        <meta property="og:title" content="@fplbot" />
+        <meta property="og:description" content="A Slack app for Fantasy Premier League" />
+        <meta property="og:image" content="https://fantasy.premierleague.com/img/share/facebook-share.png" />
       </Head>
       <Component {...pageProps} />
     </>
