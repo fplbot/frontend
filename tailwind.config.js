3a6caf45f43f70fb991728382bc3bac6b356ca42
Kristiane Westgård <kristiane.westgard@blank.no>
2021-01-27 11:11:41 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   purge: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/**/**/*.{js,ts,jsx,tsx}',
-    './src/pages/*.{js,ts,jsx,tsx}',
-    './src/components/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './pages/*.{js,ts,jsx,tsx}',
+    './components/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontFamily: {
