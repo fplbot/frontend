3d89320b177647db643304835c095cf710e5caa0
Kristiane Westgård <kristiane.westgard@blank.no>
2021-01-27 11:04:37 +0100

    add stuff to tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: [
-    './src/**/*.tsx',
-    './src/**/**/*.tsx',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/**/*.{js,ts,jsx,tsx}',
+    './src/pages/*.{js,ts,jsx,tsx}',
+    './src/components/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontFamily: {
