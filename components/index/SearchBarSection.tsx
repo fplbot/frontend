eb10b110e49c6334acbd567e57a353e8a2782ab7
Lars Skjelbek <lars.skjelbek@gmail.com>
2021-02-15 23:08:06 +0100

    Minor style tweaks

--- components/index/SearchBarSection.tsx
+++ components/index/SearchBarSection.tsx
@@ -8,14 +8,14 @@ export default function SearchBarSection() {
           FPL Search
         </h1>
         <p className="text-lg text-center text-fpl-purple mt-4 mb-4">
-          Search for people playing Fantasy Premier League
+          Search for people playing Fantasy&nbsp;Premier&nbsp;League
         </p>
         <form className="search-bar" action="/search">
           <input
             name="search"
             placeholder="Magnus Carlsen"
             aria-label="Search for fpl player"
-            className="w-72 py-2 px-4 mr-4 text-fpl-purple border-2 border-fpl-purple rounded focus:outline-none"
+            className="search-input w-72 py-2 px-4 text-fpl-purple border-2 border-fpl-purple rounded focus:outline-none"
           />
           <Button onClick={() => {}} shape="long" className="mt-4">
             Search
