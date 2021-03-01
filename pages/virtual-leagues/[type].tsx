f584fc603555c159a911854905f13c367ea2a4fb
Kristiane Westgård <kristiane.westgard@blank.no>
2021-03-01 10:10:51 +0100

    slightly better league descriptions

--- pages/virtual-leagues/[type].tsx
+++ pages/virtual-leagues/[type].tsx
@@ -1,36 +1,42 @@
 import { NextPage, NextPageContext } from "next";
 import VirtualLeaguePageContent from "../../components/virtual-leagues/VirtualLeaguePageContent";
 import { getVerifiedHelpText } from "../../services/getVerifiedHelpText";
-import { getVerifiedEntries as getVirtualLeagues, GetVerifiedEntriesResponse } from "../../services/verified";
+import {
+  getVerifiedEntries as getVirtualLeagues,
+  GetVerifiedEntriesResponse,
+} from "../../services/verified";
 import { toVerifiedType, VerifiedType } from "../../services/VerifiedType";
 
 interface PageProps {
-  res: GetVerifiedEntriesResponse,
-  verifiedType: VerifiedType
+  res: GetVerifiedEntriesResponse;
+  verifiedType: VerifiedType;
 }
 
 const VirtualLeaguePage: NextPage<PageProps> = ({ res, verifiedType }) => {
   if (res.type === "SUCCESS")
-    return <VirtualLeaguePageContent
-      title={verifiedType}
-      description={getVerifiedHelpText(verifiedType)}
-      verifiedEntries={res.data}
-      relUrl={verifiedType} />
+    return (
+      <VirtualLeaguePageContent
+        title={verifiedType}
+        description={getVerifiedHelpText(verifiedType)}
+        verifiedEntries={res.data}
+        relUrl={verifiedType}
+      />
+    );
 
   return (
     <p className="pb-16 text-lg md:text-xl text-fpl-purple text-center">
       Looks like something went wrong 🤕
     </p>
-  )
+  );
 };
 
 VirtualLeaguePage.getInitialProps = async (ctx: NextPageContext) => {
-  const verifiedType = toVerifiedType(ctx.query.type as string)
+  const verifiedType = toVerifiedType(ctx.query.type as string);
   var res = await getVirtualLeagues(verifiedType);
   return {
     res: res,
-    verifiedType: verifiedType
-  }
+    verifiedType: verifiedType,
+  };
 };
 
 export default VirtualLeaguePage;
