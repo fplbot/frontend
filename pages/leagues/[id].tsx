ee2636e5fc5b536df3b157fefe8caad2cbc53b5b
John Korsnes <johnkors@gmail.com>
2021-03-17 12:34:50 +0100

    PR-review: Adds context when errors

--- pages/leagues/[id].tsx
+++ pages/leagues/[id].tsx
@@ -6,6 +6,7 @@ import Breadcrumbs from '../../components/Breadcrumbs';
 import Footer from '../../components/Footer';
 import { getTransfersForEntries, LeagueRes, LeagueResError, EntryTransfer, http } from '../../services/leagues';
 import Button from '../../components/Button';
+import SimpleHeader from '../../components/Menu';
 
 const LeagueIndex: NextPage = () => {
 
@@ -18,10 +19,6 @@ const LeagueIndex: NextPage = () => {
 
   const [playerTransfers, setPlayerTransfers] = useState<EntryTransfer[]>([]);
   const { data, error } = useSWR<LeagueRes, LeagueResError>(`/api/fpl/leagues-classic/${id}/standings/`, http);
-
-  if (error) return (<div>Failed to load league</div>)
-  if (!data) return (<div>Loading league...</div>)
-  if (!data.league) return (<div>No league found for {id}!</div>)
 
   async function fetchTransfersClick() {
     if (data) {
@@ -39,54 +36,62 @@ const LeagueIndex: NextPage = () => {
           content={`FPL League`}
         />
       </Head>
+      <SimpleHeader />
       <div className="flex-grow px-8">
         <div className="w-full max-w-7xl m-auto mt-4 mb-8 px-8 text-center">
           <Breadcrumbs
             breadcrumbs={[
               { title: "Home", href: "/" },
               { title: "Leagues" },
-              { title: id.toString() },
+              { title: id && id != NaN ? id.toString() : '?' },
             ]}
           />
-          <h1 className="text-xl md:text-2xl font-bold text-fpl-purple mb-2">
-            {data.league.name}
-          </h1>
-          <table>
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Name</th>
-                <th>Points</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.standings.results.map((item, i) =>
-              (
-                <tr key={item.entry}>
-                  <td>{item.rank}</td>
-                  <td>{item.player_name}</td>
-                  <td>{item.total}</td>
-                </tr>
-              )
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div className="flex-grow px-8">
-        <div className="w-full max-w-7xl m-auto mt-4 mb-8 px-8 text-center">
-          <Button onClick={fetchTransfersClick} shape="square" >
-            Show all transfers!
-          </Button>
-        </div>
-      </div>
-      <div className="flex-grow px-8">
-        <div className="w-full max-w-7xl m-auto mt-4 mb-8 px-8 text-center">
-          {playerTransfers && playerTransfers.map((item, i) => (<div key={i}>{item.entry.player_name} : {item.playerOut.web_name} ➡️ {item.playerIn.web_name}</div>))}
+          {error ? (<div>Error!</div>) : (<></>)}
+          {!error && !data ? (<div>Loading league</div>) : (<></>)}
+          {!data?.league ? (<>No league found!</>) :
+            (
+              <div>
+                <h1 className="text-xl md:text-2xl font-bold text-fpl-purple mb-2">
+                  {data.league.name}
+                </h1>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Rank</th>
+                      <th>Name</th>
+                      <th>Points</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.standings.results.map((item, i) =>
+                    (
+                      <tr key={item.entry}>
+                        <td>{item.rank}</td>
+                        <td>{item.player_name}</td>
+                        <td>{item.total}</td>
+                      </tr>
+                    )
+                    )}
+                  </tbody>
+                </table>
+                <div className="flex-grow px-8">
+                  <div className="w-full max-w-7xl m-auto mt-4 mb-8 px-8 text-center">
+                    <Button onClick={fetchTransfersClick} shape="square" >
+                      Show all transfers!
+                    </Button>
+                  </div>
+                </div>
+                <div className="flex-grow px-8">
+                  <div className="w-full max-w-7xl m-auto mt-4 mb-8 px-8 text-center">
+                    {playerTransfers && playerTransfers.map((item, i) => (<div key={i}>{item.entry.player_name} : {item.playerOut.web_name} ➡️ {item.playerIn.web_name}</div>))}
+                  </div>
+                </div>
+              </div>
+            )}
         </div>
       </div>
       <Footer />
     </div>)
-}
+};
 
 export default LeagueIndex;
