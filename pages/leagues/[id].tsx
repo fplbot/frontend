cd1c5c8c94024d4ba2775dd5edbbd8746aa0f1e0
John Korsnes <johnkors@gmail.com>
2021-03-17 20:58:07 +0100

    Adds spinner to fetch transfers action on league page

--- pages/leagues/[id].tsx
+++ pages/leagues/[id].tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head';
 import { NextPage } from "next";
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr'
 import Breadcrumbs from '../../components/Breadcrumbs';
 import Footer from '../../components/Footer';
-import { getTransfersForEntries, LeagueRes, LeagueResError, Entry, EntryTransfer, http } from '../../services/leagues';
+import { getTransfersForEntries, LeagueRes, LeagueResError, EntryTransfer, http } from '../../services/leagues';
 import Button from '../../components/Button';
 import SimpleHeader from '../../components/Menu';
+import { Spinner } from '../../components/Spinner';
 
 const LeagueIndex: NextPage = () => {
 
@@ -14,42 +15,21 @@ const LeagueIndex: NextPage = () => {
     return (<></>)
   }
 
-  const segs = window?.location.pathname.split('/');
+  const segs = window.location.pathname.split('/');
   const id = parseInt(segs[segs.length - 1]);
 
-  const [playerTransfers, setPlayerTransfers] = useState<Map<string, EntryTransfer[]>>(new Map<string, EntryTransfer[]>());
   const { data, error } = useSWR<LeagueRes, LeagueResError>(`/api/fpl/leagues-classic/${id}/standings/`, http);
+  const [loading, setLoadingState] = useState<boolean>(false);
+  const [playerTransfers, setPlayerTransfers] = useState<Map<string, EntryTransfer[]>>(new Map<string, EntryTransfer[]>());
 
   async function fetchTransfersClick() {
+    setLoadingState(true);
     if (data) {
-      var transfers = await getTransfersForEntries(data.standings.results)
+      const transfers = await getTransfersForEntries(data.standings.results);
       setPlayerTransfers(transfers);
+      setLoadingState(false);
     }
   };
-
-  const items: JSX.Element[] = [];
-  playerTransfers.forEach((value: EntryTransfer[], key: string) => {
-    items.push((
-      <div key={key} className="divide-y-4 divide-fpl-purple">
-        <div className="text-left align-top font-bold">
-          {key}
-        </div>
-        <div className="text-left align-top p-3">
-          {value.length > 0 ? value.map((item, i) =>
-          (
-            <div className={`'grid grid-cols-3' ${i > 0 ? 'mt-4' : ''}`}>
-              <div className="bg-red-200 rounded-md pl-1 mb-1">{item.playerOut.web_name}</div>
-              <div className="bg-green-200 rounded-md pl-1">{item.playerIn.web_name}</div>
-            </div>
-          ))
-            :
-            (<div className="italic">No transfers</div>)
-          }
-        </div>
-      </div>
-    ));
-  });
-
 
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-tr from-white to-gray-200">
@@ -70,8 +50,12 @@ const LeagueIndex: NextPage = () => {
               { title: id && id != NaN ? id.toString() : '?' },
             ]}
           />
-          {error ? (<div>Error!</div>) : (<></>)}
-          {!error && !data ? (<div>Loading league</div>) : (<></>)}
+          {error && (<div>Error!</div>)}
+          {!error && !data && (
+            <div className="flex justify-center">
+              <Spinner size="lg" />
+            </div>
+          )}
           {!data?.league ? (<>No league found!</>) :
             (
               <div>
@@ -100,16 +84,12 @@ const LeagueIndex: NextPage = () => {
                 </table>
                 <div className="flex-grow px-8">
                   <div className="w-full max-w-7xl m-auto mt-4 mb-8 px-8 text-center">
-                    <Button onClick={fetchTransfersClick} shape="square" >
+                    <Button onClick={fetchTransfersClick} shape="square">
                       Show all transfers!
                     </Button>
                   </div>
                 </div>
-                {items.length > 0 ? (
-                  <div>
-                      {items}
-                  </div>
-                ) : (<></>)}
+                <TransfersForLeague loading={loading} playerTransfers={playerTransfers} />
               </div>
             )}
         </div>
@@ -117,5 +97,46 @@ const LeagueIndex: NextPage = () => {
       <Footer />
     </div>)
 };
+
+interface TransfersForLeagueProps {
+  loading: boolean;
+  playerTransfers: Map<string, EntryTransfer[]>;
+}
+
+function TransfersForLeague({ playerTransfers, loading }: TransfersForLeagueProps) {
+  if (loading)
+    return (
+      <div className="flex justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+
+  let items: JSX.Element[] = [];
+  playerTransfers.forEach((value: EntryTransfer[], key: string) => {
+    items.push(
+      (
+        <div key={"abc" + key + value.length} className="divide-y-4 divide-fpl-purple">
+          <div className="text-left align-top font-bold">
+            {key}
+          </div>
+          <div className="text-left align-top p-3">
+            {value.length > 0 ? value.map((item, i) =>
+            (
+              <div className={`'grid grid-cols-3' ${i > 0 ? 'mt-4' : ''}`}>
+                <div className="bg-red-200 rounded-md pl-1 mb-1">{item.playerOut.web_name}</div>
+                <div className="bg-green-200 rounded-md pl-1">{item.playerIn.web_name}</div>
+              </div>
+            ))
+              :
+              (<div className="italic">No transfers</div>)
+            }
+          </div>
+        </div>
+      )
+    );
+  });
+
+  return (<>{items}</>);
+}
 
 export default LeagueIndex;
