c829a2fc2752ab40285020e81ae230eeebe614e5
John Korsnes <johnkors@gmail.com>
2021-03-24 15:49:12 +0100

    Adds aggregated search page `searchAny` (#58)
    * Adds aggregated search page `searchAny`
    
    * PR-review: Create union type instead of inline
    
    * PR-review: split rows into components
    
    * Removes duplicate type def in json interfaces
    
    * Removes duplicate code in row

---
+++ services/searchAny.ts
@@ -0,0 +1,92 @@
+import { FPLBOT_API_BASEURL } from "../utils/envconfig";
+import { VerifiedType } from "./VerifiedType";
+
+export interface FplApiResponse {
+  hits: {
+    count: number;
+    exposedHits: Source[];
+    hitCountExceedingExposedOnes: number;
+    totalHits: number;
+    page: number;
+    totalPages: number;
+  };
+}
+
+export interface PlayerEntry  {
+  id: number;
+  realName: string;
+  teamName: string;
+  verifiedType?: VerifiedType;
+  alias: string;
+  description: string;
+}
+
+export interface LeagueEntry  {
+  id: number;
+  name: string;
+  adminEntry: number;
+  adminName: string;
+  adminTeamName: string;
+  adminCountry: string;
+}
+
+export type PlayerSource = {
+  type : "entry";
+  source : PlayerEntry;
+}
+
+export type LeagueSource = {
+  type : "league";
+  source : LeagueEntry;
+}
+
+export type Source = PlayerSource | LeagueSource;
+
+export interface SearchSuccess {
+  type: "SUCCESS";
+  data: Source[];
+  hasPrev: boolean;
+  hasNext: boolean;
+  totalPages: number;
+}
+
+interface SearchError {
+  type: "ERROR";
+}
+
+export type SearchResponse = SearchSuccess | SearchError;
+
+export function searchAny(
+  searchString: string,
+  page: number
+): Promise<SearchResponse> {
+  return fetch(
+    `${FPLBOT_API_BASEURL}/search/any?query=${searchString}&page=${page}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .then(
+      (json: FplApiResponse): SearchSuccess => {
+        return {
+          type: "SUCCESS",
+          data: json.hits.exposedHits,
+          hasPrev: json.hits.page > 0,
+          hasNext: json.hits.totalPages - 1 > json.hits.page,
+          totalPages: json.hits.totalPages,
+        };
+      }
+    )
+    .catch(
+      (error): SearchError => {
+        return { type: "ERROR" };
+      }
+    );
+}
+
