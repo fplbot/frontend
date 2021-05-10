3c7092bd62741411db359bf6aa033f4290c0e6f3
John Korsnes <johnkors@gmail.com>
2021-05-10 14:28:23 +0200

    A little bit of :sparkle:

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import Stripe from "stripe";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import Button from "../../components/Button";
 import Footer from "../../components/Footer";
@@ -7,7 +8,6 @@ import SimpleHeader from "../../components/Menu";
 import { createStripeCheckoutSession } from "../../services/stripeBrowser";
 import { getPlans, Plan } from "../../services/stripeServer";
 import getStripe from "../../utils/getStripe";
-
 
 interface CheckoutPageProps {
   plans: Plan[];
@@ -26,23 +26,56 @@ const CheckoutPage = ({ plans }: CheckoutPageProps) => (
           breadcrumbs={[{ title: "Home", href: "/" }, { title: "Checkout" }]}
         />
 
-        {plans.map((plan) => {
-          return (
-            <div className="">
-              <div>
-                <h1 className="text-3xl md:text-4xl font-bold text-fpl-purple mb-2">
-                  {plan.product.name}
-                </h1>
+        <div>
+          {plans.map((plan) => {
+            return (
+              <div className="flex flex-col items-center lg:inline-grid bg-white rounded-lg m-10">
+                <div className="p-3 m-3">
+                  <h1 className="text-3xl md:text-4xl font-bold text-fpl-purple mb-2">
+                    {plan.product.name}
+                  </h1>
+                  <div className="border border-gray-100 mb-2"></div>
+
+                  <img
+                    className="m-auto"
+                    src={plan.product.images[0]}
+                    alt={plan.product.name}
+                    width={100}
+                    height={100}
+                  />
+
+                  <div className="text-3xl pt-10">
+                    {formatAmount(plan.price.unit_amount, plan.price.currency)}
+                  </div>
+                  <span>monthly</span>
+
+                  <dl className="my-10">
+                    <MetaDataListItem
+                      title="Workspaces"
+                      metadata={plan.product.metadata}
+                    />
+                    <MetaDataListItem
+                      title="Events"
+                      metadata={plan.product.metadata}
+                    />
+                    <MetaDataListItem
+                      title="Days"
+                      metadata={plan.product.metadata}
+                    />
+                    <MetaDataListItem
+                      title="Platforms"
+                      metadata={plan.product.metadata}
+                    />
+                  </dl>
+                  <CheckoutSubscription
+                    priceId={plan.price.id}
+                    title={plan.product.name}
+                  />
+                </div>
               </div>
-              <div className="m-3 p3">
-                <CheckoutSubscription
-                  priceId={plan.price.id}
-                  title={plan.product.name}
-                />
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
 
@@ -50,13 +83,30 @@ const CheckoutPage = ({ plans }: CheckoutPageProps) => (
   </div>
 );
 
+interface MetaDataListItemProps {
+  title: string;
+  metadata: Stripe.Metadata;
+}
+
+const MetaDataListItem = ({ title, metadata }: MetaDataListItemProps) => (
+  <div className="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 border-gray-200 border-b-2 border-opacity-60">
+    <dt className="text-sm font-medium text-gray-500">{title}</dt>
+    <dd className="mt-1 font-bold text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+      {metadata[title]}
+    </dd>
+  </div>
+);
+
 CheckoutPage.getInitialProps = async () => {
   const res = await getPlans();
 
   if (res.type === "SUCCESS") {
+    const sorted = res.plans?.sort(
+      (a, b) => a.price.unit_amount! - b.price.unit_amount!
+    );
     return {
-      plans: res.plans
-    }
+      plans: sorted,
+    };
   }
 
   return {
@@ -99,3 +149,14 @@ const CheckoutSubscription = ({
     </>
   );
 };
+
+function formatAmount(amount: number | null, currency: string): string {
+  let numberFormat = new Intl.NumberFormat(["en-US"], {
+    style: "currency",
+    currency: currency,
+    currencyDisplay: "symbol",
+  });
+
+  if (amount) return numberFormat.format(amount / 100);
+  return numberFormat.format(0);
+}
