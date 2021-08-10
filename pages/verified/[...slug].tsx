26c0c691afbb5a3741482083db7f292e95b85233
Lars Skjelbek <lars.skjelbek@gmail.com>
2021-08-10 20:57:54 +0200

    Missing nullcheck

--- pages/verified/[...slug].tsx
+++ pages/verified/[...slug].tsx
@@ -103,7 +103,7 @@ const VerifiedEntryIndex: NextPage<VerifiedEntryIndexProps> = ({verifiedEntryDat
               <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">Captain</dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {verifiedEntry.captain} (VC: {verifiedEntry.viceCaptain})
+                  {verifiedEntry.captain} {verifiedEntry.viceCaptain && `(VC: ${verifiedEntry.viceCaptain})`}
                 </dd>
               </div>
               <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
