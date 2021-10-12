1184efbe774b0503dbff24856c3fe094c87d4baa
John Korsnes <johnkors@gmail.com>
2021-10-12 14:58:59 +0200

    update meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,10 @@ const Index: NextPage = () => {
     <div>
       <Head>
         <title>Fantasy Premier League Slack/Discord chatbot and search</title>
+        <meta name="title" content="Fantasy Premier League Slack/Discord chatbot and search" />
         <meta
           name="description"
-          content="Chatbot for Fantasy Premier League. Posts live gameweek updates, standings for the league you follow and more. Search for Fantasy Premier League managers or leagues. Search by name or team name, and find FPL players and celebrities."/>
+          content="Posts live gameweek updates, standings for the league you follow and more. Search for Fantasy Premier League managers or leagues. Search by name or team name, and find FPL players and celebrities."/>
       </Head>
       <Header />
       <Features />
